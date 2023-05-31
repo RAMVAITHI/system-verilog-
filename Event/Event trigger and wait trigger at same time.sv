2177a38c941ebb0c2dbe2a50dbcfee3499ba5fb2
RAMVAITHI <125638514+RAMVAITHI@users.noreply.github.com>
2023-05-31 09:11:05 +0530

    Create Event trigger and wait trigger at same time.sv

---
+++ Event/Event trigger and wait trigger at same time.sv
@@ -0,0 +1,23 @@
+module event_example();
+  event e1;
+  
+  task process_A();
+    #10;
+    $display("@%0t: Before triggering event e1", $time);
+    ->e1;
+    $display("@%0t: After triggering event e1", $time);
+  endtask
+  
+  task process_B();
+    $display("@%0t: waiting for the event e1", $time);
+    @e1;
+    $display("@%0t: event e1 is triggered", $time);
+  endtask
+
+  initial begin
+    fork
+      process_A();
+      process_B();
+    join
+  end
+endmodule
