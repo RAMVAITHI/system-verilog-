8951899e0825744ea735c12140184a4b2c387226
RAMVAITHI <125638514+RAMVAITHI@users.noreply.github.com>
2023-06-06 18:10:15 +0530

    Update Insideconstrain.sv

--- Randomisation/Insideconstrain.sv
+++ Randomisation/Insideconstrain.sv
@@ -1,9 +1,9 @@
-class PQR; 
+class abc; 
 
   // random variable declared using the rand keyword
   rand bit [3:0] var1;
 
-  //constraint block
+
   constraint C1 {(var1 inside {[3:9]});}
 
 endclass
@@ -11,24 +11,25 @@ endclass
 module top;
 initial begin
 
-int i;
-
-  //Here, we need to create handle
-  //handle name is pkt
-  PQR pqr;
-
-  //memory allocation to the handle
-  pqr=new();
-  $display("-------Output for  inside constraint-------");
-  $display("-------------------------------------------");
-  for(int i =1; i<7;i++)
-  
-  begin
-
-    // The .randomize() function is used to randomize properties of an object of a class
-    void'(pqr.randomize());
-    $display("[%0t] @ iteration: %0d -----> var1=%0d",$time,i,pqr.var1);
+abc=new();
+  repeat (5) begin;
+void'(abc.randomize());
+    $display("t= %0d,abc.var1=%0d "$time,abc.var1);
   end
   $display("-------------------------------------------");
 end
 endmodule
+
+
+
+
+
+
+
+  
+
+  
+
+
+ 
+    
